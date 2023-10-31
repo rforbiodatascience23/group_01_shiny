ed48f390a9a2e3eec233ca6e1174fae05b8e42d2
qbechv <s233374@dtu.dk>
2023-10-31 11:43:22 +0100

    .

--- R/mod_Plotting module.R
+++ R/mod_Plotting module.R
@@ -10,12 +10,13 @@
 
 mod_Plotting_module_ui <- function(id){
   ns <- NS(id)
-  tagList(h1("Group 01 Shiny App"),
-          tabsetPanel(
-            tabPanel(title = "Peptide Sequence",
-             "hey"),
-            tabPanel(title = "Plots",
-             "module2")
+  tagList(sidebarLayout(
+    sidebarPanel(
+      "peptide_sequence"
+    ),
+    mainPanel(
+      "plot"
+    )
   ))
 }
 
